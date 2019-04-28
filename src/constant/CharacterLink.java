7c3e41a25bc763c63808fe95d534f9b3d7a5f5aa
Borvornwit <petebest47@gmail.com>
2019-04-28 18:12:30 +0700

    create list of animation but not implement yet

--- src/constant/CharacterLink.java
+++ src/constant/CharacterLink.java
@@ -6,10 +6,8 @@ public class CharacterLink {
 	
 	public static Image wizardFireIcon; 
 	public static Image knight1Icon;
-	public static Image daggerUserIcon;
-	public static Image archerBlueIcon;
-	public static Image archerBlackIcon;
-	public static Image fallenAngleIcon;
+	public static Image thiefIcon;
+	public static Image archerIcon;
 	public static Image knight2Icon;
 	public static Image knight3Icon;
 	public static Image wizardLaserIcon;
@@ -19,11 +17,56 @@ public class CharacterLink {
 	public static Image troll1Icon;
 	public static Image troll2Icon;
 	public static Image orkIcon;
+	public static Image angleIcon;
+	public static Image fairyIcon;
 	
 	public static Image[] wizardFireWalk = new Image[5];
 	public static Image[] wizardFireDie = new Image[5] ;
 	public static Image[] wizardFireAttack = new Image[5] ;
-	public static Image[] knightWalk = new Image[];
+	public static Image[] knight1Walk = new Image[7];
+	public static Image[] knight1Die = new Image[7];
+	public static Image[] knight1Attack = new Image[8];
+	public static Image[] knight2Walk = new Image[7];
+	public static Image[] knight2Die = new Image[7];
+	public static Image[] knight2Attack = new Image[7];
+	public static Image[] knight3Walk = new Image[7];
+	public static Image[] knight3Die = new Image[7];
+	public static Image[] knight3Attack = new Image[7];
+	public static Image[] thiefWalk = new Image[5];
+	public static Image[] thiefDie = new Image[5];
+	public static Image[] thiefAttack = new Image[5];
+	public static Image[] archerWalk = new Image[5];
+	public static Image[] archerDie = new Image[5];
+	public static Image[] archerAttack = new Image[5];
+	public static Image[] wizardLaserWalk = new Image[5];
+	public static Image[] wizardLaserDie = new Image[5];
+	public static Image[] wizardLaserAttack = new Image[5];
+	public static Image[] laser = new Image[3];
+	public static Image[] goblinWalk = new Image[12];
+	public static Image[] goblinDie = new Image[15];
+	public static Image[] goblinAttack = new Image[12];
+	public static Image[] golemWalk = new Image[12];
+	public static Image[] golemDie = new Image[15];
+	public static Image[] golemAttack = new Image[12];
+	public static Image[] orcWalk = new Image[12];
+	public static Image[] orcDie = new Image[15];
+	public static Image[] orcAttack = new Image[12];
+	public static Image[] troll1Walk = new Image[7];
+	public static Image[] troll1Die = new Image[7];
+	public static Image[] troll1Attack = new Image[7];
+	public static Image[] troll2Walk = new Image[7];
+	public static Image[] troll2Die = new Image[7];
+	public static Image[] troll2Attack = new Image[7];
+	public static Image[] orkWalk = new Image[7];
+	public static Image[] orkDie = new Image[7];
+	public static Image[] orkAttack = new Image[7];
+	public static Image[] angleWalk = new Image[12];
+	public static Image[] angleDie = new Image[15];
+	public static Image[] angleAttack = new Image[12];
+	public static Image[] fairyWalk = new Image[5];
+	public static Image[] fairyDie = new Image[5];
+	public static Image[] fairyAttack = new Image[5];
+	
 	
 	public static void loadResource() {
 		
