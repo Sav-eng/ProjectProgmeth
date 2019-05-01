19d7a0edd4319cc407f95b60ae368296027ea840
Borvornwit <petebest47@gmail.com>
2019-05-01 18:27:11 +0700

    finish controlpane for world1

--- src/ui/hud/ControlPane.java
+++ src/ui/hud/ControlPane.java
@@ -2,10 +2,14 @@ package ui.hud;
 
 import constant.LoadResource;
 import constant.Numbers;
+import javafx.beans.binding.Bindings;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
 public class ControlPane extends HBox{
 	
@@ -26,7 +30,10 @@ public class ControlPane extends HBox{
 		this.setSpacing(10);
 		this.setPrefHeight(0.2*Numbers.WIN_HEIGHT);
 		this.setPrefWidth(Numbers.WIN_WIDTH);
-		this.setPadding(new Insets(5,0,0,0));
+		this.setPadding(new Insets(5,0,5,0));
+		this.setAlignment(Pos.BOTTOM_CENTER);
+		this.setStyle("-fx-background-color: #c19a6b;");
+
 		
 
 		
@@ -35,16 +42,21 @@ public class ControlPane extends HBox{
 		case 2 : InitializeButtonLevel2(); break;
 		}
 		
-		this.getChildren().addAll(rateUp,Char1,Char2,Char3,Char4,Char5,levelUp,attackUp,defenceUp);
+		this.getChildren().addAll(rateUp,Char1,Char2,Char3,Char4,Char5,attackUp,defenceUp,levelUp);
 	}
 	
 	public void InitializeButtonLevel1() {
 		//TODO set entityButton
-		Char1 = new EntityButton(LoadResource.knight1Icon, 100 , 1);
-		Char2 = new EntityButton(LoadResource.thiefIcon, 100, 1);
-		Char3 = new EntityButton(LoadResource.archerIcon, 100, 1);
+		Char1 = new EntityButton(LoadResource.thiefIcon, 100 , 1);
+		Char1.setStyle("-fx-background-color: radial-gradient(radius 180%, #cd7f32, derive(#cd7f32, -30%), derive(#cd7f32, 30%));");
+		Char2 = new EntityButton(LoadResource.archerIcon, 100, 1);
+		Char2.setStyle("-fx-background-color: radial-gradient(radius 180%, #cd7f32, derive(#cd7f32, -30%), derive(#cd7f32, 30%));");
+		Char3 = new EntityButton(LoadResource.knight1Icon, 100, 1);
+		Char3.setStyle("-fx-background-color: radial-gradient(radius 180%, #C0C0C0, derive(#C0C0C0, -30%), derive(#C0C0C0, 30%));");
 		Char4 = new EntityButton(LoadResource.fairyIcon, 100, 1);
+		Char4.setStyle("-fx-background-color: radial-gradient(radius 180%, #C0C0C0, derive(#C0C0C0, -30%), derive(#C0C0C0, 30%));");
 		Char5 = new EntityButton(LoadResource.wizardFireIcon, 100, 1);
+		Char5.setStyle("-fx-background-color: radial-gradient(radius 180%, #FFDF00, derive(#FFDF00, -30%), derive(#FFDF00, 30%));");
 		rateUp = new RateButton(LoadResource.rateUpCd, 300, RateButton.getLevel());
 		levelUp = new UpgradeButton(LoadResource.levelUpCd, 500, 1);
 		attackUp = new AttackBuffButton(LoadResource.atkUpCd, 200, 15, Numbers.world1BuffAmount);
@@ -60,5 +72,6 @@ public class ControlPane extends HBox{
 		rateUp = new RateButton(LoadResource.rateUpCd, 300, RateButton.getLevel());
 		attackUp = new AttackBuffButton(LoadResource.atkUpCd, 200, 20, Numbers.world2BuffAmount);
 		defenceUp = new DefenceBuffButton(LoadResource.defUpCd, 200, 20, Numbers.world2BuffAmount);
+		levelUp = new UpgradeButton(LoadResource.levelUpCd, 500, 1);
 	}
 }
--- src/ui/scene/testGameScene.java
+++ src/ui/scene/testGameScene.java
@@ -1,8 +1,12 @@
 package ui.scene;
 
 import constant.LoadResource;
+import constant.Numbers;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import ui.hud.ControlPane;
 
@@ -17,15 +21,17 @@ public class testGameScene extends Application {
 
 		// TODO Implement Main
 		LoadResource.loadResource();
-		
+		BorderPane aPane = new BorderPane();
 //		GamePlayScene root = new GamePlayScene();
 		ControlPane root = new ControlPane(1);
+		root.setAlignment(Pos.BOTTOM_CENTER);
 		
+		aPane.setBottom(root);
 		
 //		HBox root = new HBox();
 //		LoadResource.loadResource();
 		
-		Scene scene = new Scene(root);
+		Scene scene = new Scene(aPane,Numbers.WIN_WIDTH,Numbers.WIN_HEIGHT);
 		primaryStage.setTitle("test"); // Set the stage title
 		primaryStage.setScene(scene); // Place the scene
 		primaryStage.show();
