9d70b200546b358c0f7c0126b4dd8527bd2aa456
Borvornwit <petebest47@gmail.com>
2019-05-03 11:34:39 +0700

    for pull

---
+++ src/ui/scene/LoadingScene.java
@@ -0,0 +1,22 @@
+package ui.scene;
+
+import constant.LoadResource;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+
+public class LoadingScene extends StackPane {
+	 private static Canvas bg;
+	 private static GraphicsContext gContext;
+	 private static int progress = 
+	 
+	public LoadingScene() {
+		// TODO Auto-generated constructor stub
+		bg = new Canvas();
+		gContext = bg.getGraphicsContext2D();
+		
+		
+		for(int i =0: i < :i++) {
+		}
+	}
+}
